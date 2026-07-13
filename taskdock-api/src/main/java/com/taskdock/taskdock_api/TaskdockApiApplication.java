daeda3f15a1bbad120c72d72215511b636571af8
Sri Venkata Ganesh Teja Potti <ganesh.potti27@gmail.com>
2026-07-13 11:58:12 +0530

    init: spring backend setup

---
+++ taskdock-api/src/main/java/com/taskdock/taskdock_api/TaskdockApiApplication.java
@@ -0,0 +1,13 @@
+package com.taskdock.taskdock_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskdockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskdockApiApplication.class, args);
+	}
+
+}
